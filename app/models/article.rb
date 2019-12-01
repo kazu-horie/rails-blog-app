02df55e254c4023349f3e88f2161700f77d86b6a
Kazuki Horie <kazukihorie.jp@gmail.com>
2019-12-01 17:49:47 +0900

    Change ActiveRecord to Mongoid

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,10 @@
-class Article < ApplicationRecord
+class Article
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :title, type: String
+  field :description, type: String
+
   validates :title, presence: true
   validates :description, presence: true
 end
