03990407aba1391dee169a3f7298b2bd7826025d
Kazuki Horie <kazukihorie.jp@gmail.com>
2019-12-13 22:19:17 +0900

    implement activerecord cache

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,32 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def update(params)
+    super(params)
+
+    cache_params = {}
+    encode_with(cache_params)
+    Rails.cache.write(self.class.cache_key(id), cache_params)
+  end
+
+  class << self
+    def find(id)
+      if Rails.cache.exist?(cache_key(id))
+        cache_params = Rails.cache.read(cache_key(id))
+        return Article.allocate.init_with(cache_params)
+      end
+
+      record = super(id)
+
+      cache_params = {}
+      record.encode_with(cache_params)
+      Rails.cache.write(cache_key(id), cache_params)
+
+      record
+    end
+
+    def cache_key(id)
+      "#{to_s.downcase.pluralize}/#{id}"
+    end
+  end
 end
