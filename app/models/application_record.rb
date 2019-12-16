899c8035281d493d36fb7961393cef7c49ef9c62
Kazuki Horie <kazukihorie.jp@gmail.com>
2019-12-17 00:41:00 +0900

    recator record_cache

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,41 +1,5 @@
 class ApplicationRecord < ActiveRecord::Base
+  include RecordCache
+
   self.abstract_class = true
-
-  def update(params)
-    super(params)
-
-    self.class.write_cache(self)
-  end
-
-  class << self
-    def find(id)
-      record = read_cache(id)
-
-      return record if record
-
-      record = super(id)
-
-      write_cache(record)
-
-      record
-    end
-
-    def cache_key(record_id)
-      "#{to_s.downcase.pluralize}/#{record_id}"
-    end
-
-    def read_cache(record_id)
-      serialized_record = Rails.cache.read(cache_key(record_id))
-
-      return unless serialized_record
-
-      Article.allocate.init_with(serialized_record)
-    end
-
-    def write_cache(record)
-      serialized_record = {}
-      record.encode_with(serialized_record)
-      Rails.cache.write(cache_key(record.id), serialized_record)
-    end
-  end
 end
---
+++ app/models/concerns/record_cache.rb
@@ -0,0 +1,41 @@
+module RecordCache
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def find(id)
+      record = read_cache(id)
+
+      return record if record
+
+      record = super(id)
+
+      write_cache(record)
+
+      record
+    end
+
+    def cache_key(record_id)
+      "#{to_s.downcase.pluralize}/#{record_id}"
+    end
+
+    def read_cache(record_id)
+      serialized_record = Rails.cache.read(cache_key(record_id))
+
+      return unless serialized_record
+
+      Article.allocate.init_with(serialized_record)
+    end
+
+    def write_cache(record)
+      serialized_record = {}
+      record.encode_with(serialized_record)
+      Rails.cache.write(cache_key(record.id), serialized_record)
+    end
+  end
+
+  def update(params)
+    super(params)
+
+    self.class.write_cache(self)
+  end
+end
