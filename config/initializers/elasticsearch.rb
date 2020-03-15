fab04bbdadc0e2ebbe05884733b0edf74d17c638
Kazuki Horie <kazukihorie.jp@gmail.com>
2020-03-16 06:17:41 +0900

    redis, es コンテナを一時的に削除

--- config/initializers/elasticsearch.rb
+++ config/initializers/elasticsearch.rb
@@ -1,14 +1,14 @@
-config = {
-  host: ENV['ELASTICSEARCH_HOST'] || "es:9200",
-}
+# config = {
+#   host: ENV['ELASTICSEARCH_HOST'] || "es:9200",
+# }
 
-Elasticsearch::Model.client = Elasticsearch::Client.new(config)
+# Elasticsearch::Model.client = Elasticsearch::Client.new(config)
 
-begin
-  if ActiveRecord::Base.connection.table_exists? :articles
-    Article.create_index!
-    Article.__elasticsearch__.import
-  end
-rescue ActiveRecord::NoDatabaseError
-  nil
-end
+# begin
+#   if ActiveRecord::Base.connection.table_exists? :articles
+#     Article.create_index!
+#     Article.__elasticsearch__.import
+#   end
+# rescue ActiveRecord::NoDatabaseError
+#   nil
+# end
